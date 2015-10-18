29881ad155777e6a406f5d7e8206578fa7cb4deb
Federico Roffi <fedoverseas@gmail.com>
2015-10-18 23:38:24 +0100

    Added header and footer

--- src/boltomic/core.cljs
+++ src/boltomic/core.cljs
@@ -10,8 +10,46 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+
+(defn header
+  []
+  [:header.mdl-layout__header.is-casting-shadow
+   [:div.mdl-layout__header-row
+    [:span.mdl-layout-title "Boltomic"]
+    [:div.mdl-layout-spacer]
+    [:nav.mdl-navigation.mdl-layout--large-screen-only
+     [:a.mdl-navigation__link "Link"]]]])
+(defn footer
+  []
+  [:footer.mdl-mega-footer
+   [:div.mdl-mega-footer__middle-section
+    [:div.mdl-mega-footer__drop-down-section
+     [:h1.mdl-mega-footer__heading "Boltomic Ltd"]
+     [:ul.mdl-mega-footer__link-list
+      [:li "About"]]]]])
+
+
+(defn parallax
+  []
+  [:div "parallax"
+   [:img  {:src "images/barcelona.jpg"}]
+   "post-parallax"])
+
+
+(defn content
+  []
+  [:main.mdl-layout__content
+   [:div.page-content "content"
+    [parallax]]
+   [footer]])
+
+
+
 (defn hello-world []
-  [:h1 (:text @app-state)])
+  [:div.mdl-layout__container
+   [:div.mdl-layout.mdl-js-layout.mdl-layout--fixed-header.has-drawer.is-upgraded
+    [header]
+    [content]]])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
